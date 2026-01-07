4a389e62bc13eecec4b875a21a835bd9f65b7434
Assami Muzaki <72613693+Lavaruz@users.noreply.github.com>
2026-01-07 19:46:54 +0700

    feat: adding lucide angular, color palete init, home dummy content

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Circle, Calendar, Users, Heart, Instagram, Mail, MessageCircle, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-
+  readonly Circle = Circle;
+  readonly Calendar = Calendar;
+  readonly Users = Users;
+  readonly Heart = Heart;
+  readonly Instagram = Instagram;
+  readonly Mail = Mail;
+  readonly MessageCircle = MessageCircle;
 }
